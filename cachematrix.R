8a56b6779ee18e3360b6496e4fb228a41835b3cd
EraCool <EraCool@users.noreply.github.com>
2017-01-13 13:59:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -24,11 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-#The following function calculates the mean of the special "vector" created with 
-#the above function. However, it first checks to see if the mean has already 
-#been calculated. If so, it gets the mean from the cache and skips the computation. 
-#Otherwise, it calculates the mean of the data and sets the value of the mean in 
-#the cache via the setmean function.
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve retrieves the inverse from the cache
 
 # This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
